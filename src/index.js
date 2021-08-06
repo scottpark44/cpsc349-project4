232b09da273fbf5ad4671e7bfe5969ff88ab962d
Scott Park <scottpark44@csu.fullerton.edu>
2021-08-05 22:21:53 -0700

    Added Log-In Functionality and Website Public Timeline

--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
 import './tailwind.css'
 import './magnifying-glass.png'
 import './cool-background.png'
-import * as dude from './dude.js'
 import * as mockroblog from './mockroblog.js'
+
+// let wrong = document.getElementById("wrong");
+// let submit_login = document.getElementById("submit_login");
+// let submit_up = document.getElementById("submit_up");
+// if (submit_login) { submit_login.addEventListener("click",auth_login); }
+// if (submit_up) { submit_up.addEventListener("click",auth_up); }
+
+// let myStorage = window.localStorage;
+
+// export function auth_login() {
+	
+// 	let username = document.getElementById("username").value;
+// 	let password = document.getElementById("password").value; // plaintext lmao
+	
+// 	let authy = mockroblog.authenticateUser(username, password);
+
+// 	if (authy === null) { wrong.innerHTML = "<br><center>Wrong username or password</center>"; }
+// 	else { 
+// 		wrong.innerHTML = "<br><center>Logged in successfully!";
+// 		myStorage.setItem(current_user, authy.username);
+//         location.replace("./website.html");
+// 	}	
+// }
+
+// function auth_up() {
+	
+// 	let username = document.getElementById("username").value;
+// 	let email = document.getElementById("email").value;
+// 	let password = document.getElementById("password").value; // plaintext lmao
+
+// 	mockroblog.createUser(username, email, password);
+// 	location.replace("../pages/login.html");
+// }
+let myStorage = window.localStorage;
+let submit_login = document.getElementById("submit_login");
+let wrong = document.getElementById("wrong");
+if(submit_login){submit_login.addEventListener("click", auth_login);}
+
+
+
+function auth_login(){
+    let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value; // plaintext lmao
+	let authy = mockroblog.authenticateUser(username, password);
+
+	if (authy === null) { wrong.innerHTML = "<br><center>Wrong username or password</center>"; }
+	else { 
+		wrong.innerHTML = "<br><center>Logged in successfully!";
+		myStorage.setItem("current_user", authy.username);
+        console.log(myStorage.getItem("current_user"));
+        location.replace("./website.html");
+	}	
+}
+
+
+
+
+
+
--- src/signUp.js
+++ src/signUp.js
@@ -1,1 +1,27 @@
 import './tailwind.css'
+import './cool-background.png'
+import * as mockroblog from './mockroblog.js'
+
+
+let wrong = document.getElementById("wrong");
+let submit_up = document.getElementById("submit_up");
+if (submit_up) { submit_up.addEventListener("click",auth_up); }
+let myStorage = window.localStorage;
+
+
+function auth_up() {
+	let username = document.getElementById("username").value;
+	let email = document.getElementById("email").value;
+	let password = document.getElementById("password").value; // plaintext lmao
+    let password2 = document.getElementById("password2").value;
+
+    if (password != password2){
+        wrong.innerHTML = "The passwords do not match. Please check again.";
+    }
+    else{
+        mockroblog.createUser(username, email, password);
+        myStorage.setItem("current_user", username);
+        location.replace("./website.html");
+    }
+
+}
